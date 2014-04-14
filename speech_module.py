1d6bd83e639007ddfbcb038a3b46dc128979fb5d
ehnaton <ehnaton4@gmail.com>
2014-04-14 13:50:14 +0300

    solution for speech module, a dirty one

---
+++ speech_module.py
@@ -0,0 +1,55 @@
+FIRST_TEN = ["zero", "one", "two", "three", "four", "five", "six", "seven",
+             "eight", "nine"]
+SECOND_TEN = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen",
+              "sixteen", "seventeen", "eighteen", "nineteen"]
+OTHER_TENS = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy",
+              "eighty", "ninety"]
+HUNDRED = "hundred"
+
+
+def checkio(number):
+    output = []
+    if number == 0:
+        return FIRST_TEN[0]
+
+    units = number%10
+    number /= 10
+
+    tens = number % 10
+    number /= 10
+
+    hundrends = number % 10
+
+    if hundrends != 0:
+        if tens == 1:
+            return FIRST_TEN[hundrends] + " " + HUNDRED + " " + SECOND_TEN[units ]
+            print "*"
+        elif tens == 0 and FIRST_TEN[units] != "zero":
+            return FIRST_TEN[hundrends] + " " + HUNDRED + " " + FIRST_TEN[units]
+        elif tens == 0 and FIRST_TEN[units] == "zero":
+            return FIRST_TEN[hundrends] + " " + HUNDRED 
+            print "**"
+        elif tens != 1 and tens != 0 and FIRST_TEN[units] != "zero":
+            return FIRST_TEN[hundrends] + " " + HUNDRED + " " + OTHER_TENS[tens-2] + " " + FIRST_TEN[units]
+        else:
+            return FIRST_TEN[hundrends] + " " + HUNDRED + " " + OTHER_TENS[tens-2] 
+            print "***"
+    elif hundrends == 0:
+        if tens != 1 and tens != 0:
+            if FIRST_TEN[units] == "zero":
+                return OTHER_TENS[tens-2] 
+            else:
+                return OTHER_TENS[tens-2] + " " + FIRST_TEN[units]
+            print "#"
+        elif tens == 0:
+            return FIRST_TEN[units]
+            print "##"
+        else:
+            return SECOND_TEN[units]
+            print "###"
+
+
+    #print output
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print checkio(100)
